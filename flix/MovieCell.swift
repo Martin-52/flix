90f73b5b436f1ce3c0b19e3e4cfddb2bb9e622f5
Martin Landin <martinlandin@martins-macbook-air.local>
2019-02-02 18:48:24 -0800

    flix app part 1 completed

---
+++ flix/MovieCell.swift
@@ -0,0 +1,34 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Martin Landin on 1/28/19.
+//  Copyright © 2019 Martin Landin. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
