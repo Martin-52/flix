1729b257814f554a7d8c72436ee572bf64717189
Martin Landin <martinlandin@martins-macbook-air.local>
2019-02-04 15:46:10 -0800

    Completed flix Part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Martin Landin on 2/4/19.
+//  Copyright © 2019 Martin Landin. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
