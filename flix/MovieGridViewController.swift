1aa9856a4c840226ada8e8c37bd4820adf82183b
Martin Landin <martinlandin@martins-macbook-air.local>
2019-02-05 19:15:41 -0800

    Added Movie Details to Superhero Tab

--- flix/MovieGridViewController.swift
+++ flix/MovieGridViewController.swift
@@ -73,14 +73,23 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Find the Selected Movie
+        
+        print("Loading up the details screen")
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie details to the view controller
+        let detailsViewController = segue.destination as! MovieGridDetailsViewController
+        detailsViewController.movie = movie
+        
     }
-    */
+    
 
 }
